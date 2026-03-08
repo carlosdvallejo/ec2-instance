11228b179e7d6c91a114ca839ff2b38b0b4a59e3
carlosd.vallejo <cdvallejo@gmail.com>
2026-03-07 21:09:37 -0600

    last

--- cicd/configure-named-profile.sh
+++ cicd/configure-named-profile.sh
@@ -2,11 +2,12 @@
 
 set -eu
 
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile cicd-profile
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile cicd-profile
-aws configure set region "us-east-2" --profile cicd-profile 
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile $PROFILE_NAME
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile $PROFILE_NAME
+aws configure set region "us-east-2" --profile $PROFILE_NAME
 
-aws configure list --profile cicd-profile
-aws sts get-caller-identity --profile cicd-profile
+aws configure list --profile $PROFILE_NAME
+aws sts get-caller-identity --profile $PR
 
-output=$(aws sts get-caller-identity --profile cicd-profile)
+
+output=$(aws sts get-caller-identity --profile $PROFILE_NAME)
